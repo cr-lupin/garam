099102faf1f3d2532f935872b1ba18b7551f12f5
Zalan Szugyi <zalan.szugyi@gmail.com>
2014-10-08 11:50:33 +0200

    sg is going to be shown

--- client/js/main.js
+++ client/js/main.js
@@ -1,11 +1,23 @@
+/** @jsx React.DOM */
 
+var HikeList = React.createClass({
+	render: function() {
+		var hikeList = [];
+		this.props.hikes.forEach( function(hike) {
+			hikeList.push( <span> {hike.name} </span> );
+		});
+		return (
+			<li> {hikeList} </li>
+		);
+	}
+});
 
 var HikesInOneDay = React.createClass({
 	render: function() {
 		return (
 			<li class="hikesInOneDayList">
-				<div class="dateRow"> { this.props.date } </div>
-				<HikeList hikes={this.props.hikes>
+				<div class="dateRow"> { this.props.hikesInOneDay.date } </div>
+				<HikeList hikes={this.props.hikesInOneDay.hikes} />
 			</li> 
 		);
 	}
@@ -14,7 +26,7 @@ var HikesInOneDay = React.createClass({
 var HikesInOneDayList = React.createClass({
 	render: function() {
 		var hikesInOneDayList = [];
-		this.props.all.forEach(function(hikesInOneDay) {
+		this.props.hikesPerDayListData.forEach( function(hikesInOneDay) {
 				hikesInOneDayList.push(<HikesInOneDay hikesInOneDay={hikesInOneDay} />);
 		});
 		return (
@@ -29,7 +41,7 @@ var MainContent = React.createClass({
     render: function() {
         return (
 					<div class="mainContent">
-            <HikesInOneDayList hikes={this.props.all}
+            <HikesInOneDayList hikesPerDayListData={this.props.hikesPerDayListData} />
         	</div>
         );
     }
@@ -41,8 +53,9 @@ var ALL = [
 	{ date: '2014.09.27',
 		hikes: [ { name: 'tura1', id: '1'}, { name: 'tura2', id: '2'}, { name: 'tura3', id: '3'} ]
 	},
-	{ date: '2014.09.27',
+	{ date: '2014.09.28',
 		hikes: [ { name: 'tura4', id: '4'}, { name: 'tura5', id: '5'}, { name: 'tura6', id: '6'} ]
 	} ];
-	
-React.renderComponent(<MainContent products={PRODUCTS} />, document.body);
+
+React.renderComponent(<MainContent hikesPerDayListData={ALL} />, document.body);
+//React.renderComponent(<span>almafa2</span>, document.body);
