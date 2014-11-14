3f59eee2b29b3ed57a1a0e5791459b9942a1af32
Szugyi Zalan <lupin@ludens.elte.hu>
2014-11-14 16:36:06 +0100

    detailed hike

--- client/js/main.js
+++ client/js/main.js
@@ -1,106 +1,146 @@
 /** @jsx React.DOM */
 
+var SubHikeItem = React.createClass({
+  render: function() {
+    return (
+      <div className="subHikeItem"> 
+        <div className="subHikeHeader">
+          <span className="subHikeName"> {this.props.subhike.subname} </span> 
+          <span className="subHikeDate"> {this.props.subhike.date} </span> 
+        </div>
+        <div className="subHikeContent">
+          <div className="subHikeStart"> <span className="label">rajt: </span> 
+                                         <span className="content"> {this.props.subhike.start} </span></div> 
+          <div className="subHikeEnd"> <span className="label">cél: </span> 
+                                       <span className="content"> {this.props.subhike.end} </span> </div> 
+          <div className="subHikeOtherData">                    
+            <span className="image"><img src="assets/time.gif" /> </span>
+            <span className="subHikeTime"> {this.props.subhike.time} </span> 
+            <span className="placeholder" />
+            <span className="image"><img src="assets/price.gif" /> </span>
+            <span className="subHikePrice"> {this.props.subhike.price} </span> 
+            <span className="placeholder" />
+            <span className="image"><img src="assets/distance.gif" /> </span>
+            <span className="subHikeDistance"> {this.props.subhike.distance} </span>             
+            <span className="placeholder" />
+            <span className="image"><img src="assets/elevation.gif" /> </span>
+            <span className="subHikeElevation"> {this.props.subhike.elevation} </span>
+            <span className="placeholder" />
+            <span className="image"><img src="assets/limit.gif" /> </span>
+            <span className="subHikeLimit"> {this.props.subhike.limit} </span> 
+            <span className="placeholder" />
+            <span className="subHikeCaffee"> {this.props.subhike.caffee == "yes" ? "van kávé :)" : "nincs kv" } </span> 
+          </div>
+        </div>
+      </div>
+      
+    )
+  }
+});
+
+var SubHikeGroup = React.createClass({
+  render: function() {
+    var subHikeList = [];
+    this.props.subhikes.forEach( function(subhike) {
+      subHikeList.push( <SubHikeItem subhike={subhike}/> );
+    });
+    return (      
+      <ul className="subHikeList"> {subHikeList} </ul>
+    );
+  }
+});
 
 var HikeItemDetails = React.createClass({
-	render: function() {
-		return (
-			<div className="hikeItemDetail" id={this.props.hikeItemDetails.id}> 
-				<span className="hikeItemDetailName"> {this.props.hikeItemDetails.name} </span> 
-				<span className="hikeItemDetailDate"> {this.props.hikeItemDetails.date} </span> 
-				<span className="hikeItemDetailStart"> {this.props.hikeItemDetails.start} </span> 
-				<span className="hikeItemDetailEnd"> {this.props.hikeItemDetails.end} </span> 
-				<span className="hikeItemDetailDistance"> {this.props.hikeItemDetails.distance} </span> 
-				<span className="hikeItemDetailPrice"> {this.props.hikeItemDetails.price} </span> 
-				<span className="hikeItemDetailElevation"> {this.props.hikeItemDetails.elevation} </span> 
-				<span className="hikeItemDetailCaffee"> {this.props.hikeItemDetails.caffee == "yes" ? "van kávé :)" : "nincs kv" } </span> 
-			</div>
-			
-		)
-	}
+  render: function() {      
+    return (
+      <div className="hikeItemDetails">
+        <div className="hikeItemDetailName" id={this.props.hikeItemDetails.id}> {this.props.hikeItemDetails.name} </div>
+        <SubHikeGroup className="subHikeGroup" subhikes={this.props.hikeItemDetails.subhikes}/> 
+      </div>
+    );
+  }
 });
 
 var HikeItemOverall = React.createClass({
-	render: function() {
-		return (
-			<div className="hikeItemOverall" id={this.props.hikeItemOverall.id}> {this.props.hikeItemOverall.name} </div>
-		);
-	}
+    render: function() {
+      return (
+        <div className="hikeItemOverall" id={this.props.hikeItemOverall.id}> {this.props.hikeItemOverall.name} </div>
+      );
+    }
 });
 
 var HikeItem = React.createClass({
-	getInitialState: function() {
-		return {detail: false};
-	},
-	handleClick: function(event){
-		$.ajax({
-			url: 'test/hikedata.json',
-			dataType: 'json',
-			success: function(data) {      
-
-				this.setState({hikeData: data.all, detail: !this.state.detail});
-			}.bind(this),
-			error: function(xhr, status, err) {
-				console.error(this.props.url, status, err.toString());
-			}.bind(this)
-		});
-	},
-	render: function() {
-		var result = this.state.detail ?
-  		<HikeItemDetails hikeItemDetails={this.state.hikeData}/>:
-			<HikeItemOverall hikeItemOverall={this.props.hikeItem} />
-		return (
-			<li className="hikeItem" onClick={this.handleClick}> {result} </li>
-		);
-	}
+  getInitialState: function() {
+    return {detail: false};
+  },
+  handleClick: function(event){
+    $.ajax({
+      url: 'test/hikedata.json',
+      dataType: 'json',
+      success: function(data) {      
+        this.setState({hikeData: data.all, detail: !this.state.detail});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  render: function() {
+    var result = this.state.detail ?
+      <HikeItemDetails hikeItemDetails={this.state.hikeData}/>:
+      <HikeItemOverall hikeItemOverall={this.props.hikeItem} />
+    return (
+      <li className="hikeItem" onClick={this.handleClick}> {result} </li>
+    );
+  }
 });
 
 var HikeList = React.createClass({
-	render: function() {
-		var hikeList = [];
-		this.props.hikes.forEach( function(hike) {
-			hikeList.push( <HikeItem key={hike.id} hikeItem={hike}/> );
-		});
-		return (
-			<ul className="hikeList"> {hikeList} </ul>
-		);
-	}
+  render: function() {
+    var hikeList = [];
+    this.props.hikes.forEach( function(hike) {
+      hikeList.push( <HikeItem key={hike.id} hikeItem={hike}/> );
+    });
+    return (
+      <ul className="hikeList"> {hikeList} </ul>
+    );
+  }
 });
 
 var HikesInOneDay = React.createClass({
-	render: function() {
-		return (
-			<li className="hikesInOneDayList">
-				<div className="dateRow"> { this.props.hikesInOneDay.date } </div>
-				<HikeList hikes={this.props.hikesInOneDay.hikes} />
-			</li> 
-		);
-	}
+  render: function() {
+    return (
+      <li className="hikesInOneDayList">
+        <div className="dateRow"> { this.props.hikesInOneDay.date } </div>
+        <HikeList hikes={this.props.hikesInOneDay.hikes} />
+      </li> 
+    );
+  }
 });
 
 var HikesInOneDayList = React.createClass({
-	render: function() {
-		var hikesInOneDayList = [];
-		this.props.hikesPerDayListData.forEach( function(hikesInOneDay) {
-				hikesInOneDayList.push(<HikesInOneDay hikesInOneDay={hikesInOneDay} />);
-		});
-		return (
-			<ul className="hikesInOneDayList">
-				{hikesInOneDayList}    
-			</ul>
-		);
-	}
+  render: function() {
+    var hikesInOneDayList = [];
+    this.props.hikesPerDayListData.forEach( function(hikesInOneDay) {
+        hikesInOneDayList.push(<HikesInOneDay hikesInOneDay={hikesInOneDay} />);
+    });
+    return (
+      <ul className="hikesInOneDayList">
+        {hikesInOneDayList}    
+      </ul>
+    );
+  }
 });
 
 var MainContent = React.createClass({
-	getInitialState: function() {
+  getInitialState: function() {
     return {data: []};
   },
   componentDidMount: function() {
     $.ajax({
       url: this.props.url,
       dataType: 'json',
-      success: function(data) {
-      	console.error(data);
+      success: function(data) {        
         this.setState({data: data.all});
       }.bind(this),
       error: function(xhr, status, err) {
@@ -108,24 +148,24 @@ var MainContent = React.createClass({
       }.bind(this)
     });
   },
-	render: function() {
-			return (
-				<div className="mainContent">
-					<HikesInOneDayList hikesPerDayListData={this.state.data} />
-				</div>
-			);
-	}
+  render: function() {
+      return (
+        <div className="mainContent">
+          <HikesInOneDayList hikesPerDayListData={this.state.data} />
+        </div>
+      );
+  }
 });
 
 
 
 var ALL = [
-	{ date: '2014.09.27',
-		hikes: [ { name: 'tura1', id: '1'}, { name: 'tura2', id: '2'}, { name: 'tura3', id: '3'} ]
-	},
-	{ date: '2014.09.29',
-		hikes: [ { name: 'tura4', id: '4'}, { name: 'tura5', id: '5'}, { name: 'tura62', id: '62'} ]
-	} ];
+  { date: '2014.09.27',
+    hikes: [ { name: 'tura1', id: '1'}, { name: 'tura2', id: '2'}, { name: 'tura3', id: '3'} ]
+  },
+  { date: '2014.09.29',
+    hikes: [ { name: 'tura4', id: '4'}, { name: 'tura5', id: '5'}, { name: 'tura62', id: '62'} ]
+  } ];
 
 
 
